24b86cc84912f6d63d35792746c04276e9dfe8a5
sangeeta sharma <36931060+11612200@users.noreply.github.com>
2018-03-27 01:44:33 +0530

    Update project.c

--- project.c
+++ project.c
@@ -2,27 +2,27 @@
 #include<unistd.h>
 main()
 {
-int bt[10],at[10],rt[10],q[10,p[10]; // array for burst time ,arrival time  and remaining time, queue
+int count,bt[10],at[10],rt[10],q[10,p[10]; // array for burst time ,arrival time  and remaining time, queue
 int q1[10],q2[10],l=0,m=0;
 float tournaround=0,waiting=0;
 int student,faculty,total,time=0,tq,d;
 printf("\nEnter the total number of people");
 scanf("%d",&total);
 int i,j,temp;
-	for(i=0;i<total;i++)
+	for(count=0;count<total;count++)
 	{
 	printf("\nYou are student(0) or Faculty(1)");
 	scanf("%d",&d);
-	p[i]=d;
+	p[count]=d;
 	if(d==0)
 	l++;
 	else
 	{m++;}
-	printf("\nEnter the burst time for p%d",i);
-	scanf("%d",&bt[i];
-	printf("\nEnter the arrival time for p%d",i);
-	scanf("%d",&at[i];
-	rt[i]=bt[i];
+	printf("\nEnter the burst time for p%d",count);
+	scanf("%d",&bt[count];
+	printf("\nEnter the arrival time for p%d",count);
+	scanf("%d",&at[count];
+	rt[count]=bt[count];
 	}
 	
 	printf("\nEnter the Time Quantum: ");
@@ -50,4 +50,13 @@ int i,j,temp;
 	t++;
 	if(p[i]==0)
 	rt[i]=bt[i]-tq;	
+	push(p[i]);
+	if(time==at[i])
+	{
+	if(p[i]==0)
+	{
+	push(p[i])
+	}
+	else {push1(p[i])}
+	}
 }
